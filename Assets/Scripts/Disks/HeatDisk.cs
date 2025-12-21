c4802d5ab22cdb9d16132c0ba82ae1d4523d2d21
Canakklc <Cananonun@gmail.com>
2025-12-21 19:41:24 +0200

    some disks and blood showing on cam

--- Assets/Scripts/Disks/HeatDisk.cs
+++ Assets/Scripts/Disks/HeatDisk.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
@@ -8,9 +10,14 @@ public class HeatDisk : MonoBehaviour
     CCTVandCAM takeBool;
 
     public bool thermalActive;
+    public bool uvActive;
+    public bool motionSensor;
+    public GameObject motionSensorImage;
 
 
     Vector4 lockedLift = new Vector4(-0.2f, -0.2f, 0.4f, -0.6f);
+    Vector4 lockedLiftUv = new Vector4(0.36f, -0.35f, 0.5f, -0.6f);
+    Vector4 lockedLiftMotionSensor = new Vector4(0.36f, 1.0f, 0.1f, -0.18f);
 
     void Awake()
     {
@@ -22,23 +29,78 @@ public class HeatDisk : MonoBehaviour
 
         takeBool = GetComponent<CCTVandCAM>();
     }
+    void Start()
+    {
+        motionSensorImage.SetActive(false);
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T) && takeBool.onCCTV)
+        if (Input.GetKeyDown(KeyCode.T) && takeBool.onCCTV && !uvActive && !motionSensor)
         {
+            StartPostExposure();
             thermalActive = !thermalActive;
+            uvActive = false;
+            motionSensor = false;
+        }
+        if (Input.GetKeyDown(KeyCode.U) && takeBool.onCCTV == true && !thermalActive && !motionSensor)
+        {
+            StartPostExposure();
+            uvActive = !uvActive;
+            thermalActive = false;
+            motionSensor = false;
+        }
+        if (Input.GetKeyDown(KeyCode.V) && takeBool.onCCTV == true && !thermalActive && !uvActive)
+        {
+            StartPostExposure();
+            motionSensor = !motionSensor;
+            uvActive = false;
+            thermalActive = false;
         }
 
-        if (thermalActive)
+        if (thermalActive)//thermalDisk
         {
             colorGrading.lift.overrideState = true;
             colorGrading.lift.value = lockedLift;
         }
+
+        else if (uvActive)//uvDisk
+        {
+            colorGrading.lift.overrideState = true;
+            colorGrading.lift.value = lockedLiftUv;
+        }
+        else if (motionSensor)//motionSensorActive
+        {
+            colorGrading.lift.overrideState = true;
+            colorGrading.lift.value = lockedLiftMotionSensor;
+            motionSensorImage.SetActive(true);
+        }
+
         else
         {
             colorGrading.lift.overrideState = false;
+            motionSensorImage.SetActive(false);
         }
+    }
+
+    IEnumerator PostExposureLevel()
+    {
+        float makeItBright = colorGrading.postExposure.value = 0;
+        float makeItDark = colorGrading.postExposure.value = -10;
+        float Elapsed = 0;
+        float Duration = 1;
+        while (Elapsed < Duration)
+        {
+            Elapsed += Time.deltaTime;
+            float t = Elapsed / Duration;
+            t = Mathf.SmoothStep(0f, 1f, t);
+            colorGrading.postExposure.value = Mathf.Lerp(makeItDark, makeItBright, t);
+            yield return null;
+        }
+    }
+    void StartPostExposure()
+    {
+        StartCoroutine(PostExposureLevel());
     }
 
 }
---
+++ Assets/Scripts/UvObjects.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UvObjects : MonoBehaviour
+{
+    HeatDisk diskBools;
+    public List<GameObject> Objects = new List<GameObject>();
+    public float timerForSpawn = 0;
+
+
+    void Start()
+    {
+        for (int i = 0; i < Objects.Count; i++)
+        {
+            Objects[i].SetActive(false);
+        }
+    }
+    void Awake()
+    {
+        diskBools = GetComponent<HeatDisk>();
+    }
+    void Update()
+    {
+        timerForSpawn += Time.deltaTime;
+        AppearObjectsOnBool();
+    }
+    void AppearObjectsOnBool()
+    {
+        if (diskBools.uvActive)
+        {
+            for (int i = 0; i < Objects.Count; i++)
+            {
+                if (timerForSpawn >= 20f)
+                {
+                    Objects[0].SetActive(true);
+                }
+                if (timerForSpawn >= 35)
+                {
+                    Objects[1].SetActive(true);
+                }
+                if (timerForSpawn >= 42)
+                {
+                    Objects[2].SetActive(true);
+                }
+            }
+        }
+
+        else
+        {
+            for (int i = 0; i < Objects.Count; i++)
+            {
+                Objects[i].SetActive(false);
+            }
+        }
+    }
+}
